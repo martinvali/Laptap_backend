398e3d192ab4d39dff527bcdf21dbe9c6fc6763c
Martin Väli <martinvali12@gmail.com>
2021-11-04 19:56:14 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const stripe = require("stripe")(
+  "sk_test_51JktFJH7IkGhuWpe6Emppfpum3BPubj2gB9qf9aQAvuKMPBSbKdxyTqTWwFdn2jh4Qo9ndrjfO073oUzRTJh4jCF0071aQ6keV"
+);
+const express = require("express");
+
+const app = express();
+
+const port = "";
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: "price_1Js9jXH7IkGhuWpe64ZvME16",
+        quantity: 1,
+      },
+    ],
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `https://www.delfi.ee/`,
+    cancel_url: `https://www.postimees.ee/`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(3000, function () {
+  console.log("LIstening on port 3000");
+});

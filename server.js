c9f758c80c0656fe0ff2df8766e1acf170f18701
Martin Väli <martinvali12@gmail.com>
2021-11-05 20:22:42 +0200

    Change amount

--- server.js
+++ server.js
@@ -6,13 +6,10 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const price = 49;
-const calculateOrderAmount = (quantity = 1) => {
-  return quantity * price;
-};
+
 app.get("/create-checkout-session", async (req, res) => {
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 49,
+    amount: 5000,
     currency: "eur",
     payment_method_types: [
       "giropay",

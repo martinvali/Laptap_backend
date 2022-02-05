336ee02f51426a71f2503f0f4cb28f33af84cb9b
Martin Väli <martinvali12@gmail.com>
2022-02-05 19:14:59 +0200

    fix a typo that likely caused the bug

--- server.js
+++ server.js
@@ -54,12 +54,11 @@ app.post("/discount-code/:id", async function (req, res) {
   if (discountCode.length === 1) {
     const transportPrice = calculateTransportPrice(transport);
     const productsPrice = calculateProductsPrice(quantity);
-    const discount = discountcode.discount;
+    const discount = discountCode.discount;
     const paymentIntent = await stripe.paymentIntents.update(id, {
       amount: calculateTotalPrice(quantity, transport, discount),
     });
 
-    console.log(discount);
     res.send({
       quantity,
       id: paymentIntent.id,
